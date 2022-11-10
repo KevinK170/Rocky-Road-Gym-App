b28b0703235070a6ae251dfbd5eab4f36e11741f
Kevin Kolkman <48731440+KevinK170@users.noreply.github.com>
2022-11-10 14:16:39 -0800

    Update db_credentials.py

--- database/db_credentials.py
+++ database/db_credentials.py
@@ -5,10 +5,10 @@
 # You can also use environment variables
 
 # For Local Devlelopment
-host = 'localhost'
-user = 'root'                                   # can be different if you set up another username in your MySQL installation
-passwd = 'nottellingyou'                        # set accordingly
-db = 'bsg'
+host = 'classmysql.engr.oregonstate.edu'
+user = 'cs340_kolkmank'                                   # can be different if you set up another username in your MySQL installation
+passwd = '5740'                        # set accordingly
+db = 'cs340_kolkmank'
 
 
 # For OSU Flip Servers
