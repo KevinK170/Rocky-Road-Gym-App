a492f8b20d41148475770b5bf2a70a4669dd4208
mnell3 <65864450+mnell3@users.noreply.github.com>
2022-11-10 17:46:50 -0800

    edit and delete functions updated

--- app.py
+++ app.py
@@ -77,84 +77,54 @@ def rental_order():
 
         # render edit_people page passing our query data and homeworld data to the edit_people template
         return render_template("rental_orders.j2", data=data, orders = order_data, rentals=rental_data)
+    
 
-
-# # route for delete functionality, deleting a person from bsg_people,
-# # we want to pass the 'id' value of that person on button click (see HTML) via the route
-# @app.route("/delete_people/<int:id>")
-# def delete_people(id):
-#     # mySQL query to delete the person with our passed id
-#     query = "DELETE FROM bsg_people WHERE id = '%s';"
-#     cur = mysql.connection.cursor()
-#     cur.execute(query, (id,))
-#     mysql.connection.commit()
-
-#     # redirect back to people page
-#     return redirect("/people")
+# # route for delete functionality, deleting a rental order,
+# # we want to pass the 'id' value of that order on button click (see HTML) via the route
+@app.route("/delete_rentalorder/<int:id>")
+def delete_people(id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Rental_Orders WHERE rentalorder_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id))
+    mysql.connection.commit()
+    # redirect back to people page
+    return redirect("/Rental_Orders")
 
 
 # # route for edit functionality, updating the attributes of a person in bsg_people
 # # similar to our delete route, we want to the pass the 'id' value of that person on button click (see HTML) via the route
-# @app.route("/edit_people/<int:id>", methods=["POST", "GET"])
-# def edit_people(id):
-#     if request.method == "GET":
-#         # mySQL query to grab the info of the person with our passed id
-#         query = "SELECT * FROM bsg_people WHERE id = %s" % (id)
-#         cur = mysql.connection.cursor()
-#         cur.execute(query)
-#         data = cur.fetchall()
+@app.route("/edit_rentalorder/<int:id>", methods=["POST", "GET"])
+def edit_rental_order(id):
+    if request.method == "GET":
+        # mySQL query to grab the info of the rental order with our passed id
+        query = "SELECT * FROM rental_orders WHERE rentalorder_id = '%s'" % (id)
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
 
-#         # mySQL query to grab planet id/name data for our dropdown
-#         query2 = "SELECT id, name FROM bsg_planets"
-#         cur = mysql.connection.cursor()
-#         cur.execute(query2)
-#         homeworld_data = cur.fetchall()
+        # render edit_rental_order page passing our query data to the edit_rental_order
+        return render_template("rental_orders.j2", data=data)
 
-#         # render edit_people page passing our query data and homeworld data to the edit_people template
-#         return render_template("edit_people.j2", data=data, homeworlds=homeworld_data)
+    # meat and potatoes of our update functionality
+    if request.method == "POST":
+        # fire off if user clicks the 'Edit Rental Order' button
+        if request.form.get("Edit_Rental_Order"):
+            # grab user form inputs
+            id = request.form["rentalorder_id"]
+            orderID = request.form["order_id"]
+            rentalID = request.form["rental_id"]
+            query = "UPDATE Rental_Orders SET rental_id = '%s';"
+            query = "UPDATE Rental_Orders SET order_id = '%s';"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (orderID, rentalID, id))
+            mysql.connection.commit()
 
-#     # meat and potatoes of our update functionality
-#     if request.method == "POST":
-#         # fire off if user clicks the 'Edit Person' button
-#         if request.form.get("Edit_Person"):
-#             # grab user form inputs
-#             id = request.form["personID"]
-#             fname = request.form["fname"]
-#             lname = request.form["lname"]
-#             homeworld = request.form["homeworld"]
-#             age = request.form["age"]
+            # redirect back to people page after we execute the update query
+            return redirect("/Rental_Orders")
 
-#             # account for null age AND homeworld
-#             if (age == "" or age == "None") and homeworld == "0":
-#                 # mySQL query to update the attributes of person with our passed id value
-#                 query = "UPDATE bsg_people SET bsg_people.fname = %s, bsg_people.lname = %s, bsg_people.homeworld = NULL, bsg_people.age = NULL WHERE bsg_people.id = %s"
-#                 cur = mysql.connection.cursor()
-#                 cur.execute(query, (fname, lname, id))
-#                 mysql.connection.commit()
 
-#             # account for null homeworld
-#             elif homeworld == "0":
-#                 query = "UPDATE bsg_people SET bsg_people.fname = %s, bsg_people.lname = %s, bsg_people.homeworld = NULL, bsg_people.age = %s WHERE bsg_people.id = %s"
-#                 cur = mysql.connection.cursor()
-#                 cur.execute(query, (fname, lname, age, id))
-#                 mysql.connection.commit()
 
-#             # account for null age
-#             elif age == "" or age == "None":
-#                 query = "UPDATE bsg_people SET bsg_people.fname = %s, bsg_people.lname = %s, bsg_people.homeworld = %s, bsg_people.age = NULL WHERE bsg_people.id = %s"
-#                 cur = mysql.connection.cursor()
-#                 cur.execute(query, (fname, lname, homeworld, id))
-#                 mysql.connection.commit()
-
-#             # no null inputs
-#             else:
-#                 query = "UPDATE bsg_people SET bsg_people.fname = %s, bsg_people.lname = %s, bsg_people.homeworld = %s, bsg_people.age = %s WHERE bsg_people.id = %s"
-#                 cur = mysql.connection.cursor()
-#                 cur.execute(query, (fname, lname, homeworld, age, id))
-#                 mysql.connection.commit()
-
-#             # redirect back to people page after we execute the update query
-#             return redirect("/people")
 
 
 # Listener
