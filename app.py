1589c2b7071abfc60add219acd1d03956d8ded73
Kevin Kolkman <48731440+KevinK170@users.noreply.github.com>
2022-11-16 13:22:14 -0800

    Finished implementation for Rental_Equipment, Employees, and Orders

--- app.py
+++ app.py
@@ -17,19 +17,17 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 mysql = MySQL(app)
 
 # Routes
-# have homepage route to /people by default for convenience, generally this will be your home route with its own template
 @app.route("/")
 def home():
     return redirect("/Rental_Orders")
 
 
-# route for people page
+# route for rental_orders page
 @app.route("/Rental_Orders", methods=["POST", "GET"])
 def rental_order():
     # Separate out the request methods, in this case this is for a POST
-    # insert a person into the bsg_people entity
     if request.method == "POST":
-        # fire off if user presses the Add Person button
+        # fire off if user presses the Add rental_order button
         if request.form.get("Add_Rental_Order"):
             # grab user form inputs
             order_id = request.form["order_id"]
@@ -52,7 +50,7 @@ def rental_order():
 
             mysql.connection.commit()
 
-            # redirect back to people page
+            # redirect back to rental orders page
             return redirect("/Rental_Orders")
 
     # Grab Rental_Orders data so we send it to our template to display
@@ -75,7 +73,7 @@ def rental_order():
         cur.execute(query2)
         rental_data = cur.fetchall()
 
-        # render edit_people page passing our query data and homeworld data to the edit_people template
+        # render rental orders page passing our query data and homeworld data to the edit_people template
         return render_template("rental_orders.j2", data=data, orders = order_data, rentals=rental_data)
     
 
@@ -117,11 +115,9 @@ def edit_rental_order(id):
 
         # render edit_rental_order page passing our query data to the edit_rental_order
         return render_template("edit_rentalorders.j2", data=data, orders = order_data, rentals = rental_data)
-    print(request.method)
     # meat and potatoes of our update functionality
     if request.method == "POST":
         # fire off if user clicks the 'Edit Rental Order' button
-        print(request.form)
         if request.form.get("Edit_Rental_Order"):
             # grab user form inputs
             rentalorder_id = request.form["rentalorder_id"]
@@ -143,10 +139,279 @@ def edit_rental_order(id):
             cur.execute(query)
             cur.close()
 
-            # redirect back to people page after we execute the update query
+            # redirect back to rental orders page after we execute the update query
             return redirect("/Rental_Orders")
+
+# route for employees page
+@app.route("/Employees", methods=["POST", "GET"])
+def employee():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a person into the Employees entity
+    if request.method == "POST":
+        # fire off if user presses the Add Person button
+        if request.form.get("Add_Employee"):
+            # grab user form inputs
+            employee_name = request.form["employee_name"]
+
+            query = "SET FOREIGN_KEY_CHECKS=0;"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            cur.close()
+
+            query = "INSERT INTO Employees (employee_name) VALUES (%s);"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (employee_name,))
+            cur.close()
+
+            query = "SET FOREIGN_KEY_CHECKS=1;"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            cur.close()
+
+            mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/Employees")
+
+    # Grab Employees data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the orders in Employees
+        query = "SELECT * FROM Employees;"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render employees page passing our query data
+        return render_template("employees.j2", data=data)
+    
+
+# # route for delete functionality, deleting a rental order,
+# # we want to pass the 'id' value of that order on button click (see HTML) via the route
+@app.route("/delete_employee/<int:id>")
+def delete_employee(id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Employees WHERE employee_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id,))
+    mysql.connection.commit()
+    # redirect back to people page
+    return redirect("/Employees")
+
+
+# route for rental_equipment page
+@app.route("/Rental_Equipment", methods=["POST", "GET"])
+def rental_equipment():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a person into the Rental_Equipment entity
+    if request.method == "POST":
+        # fire off if user presses the Add Equipment button
+        if request.form.get("Add_Rental_Equipment"):
+            # grab user form inputs
+            rental_name = request.form["rental_name"]
+            rental_cost = request.form["rental_cost"]
+
+            query = "SET FOREIGN_KEY_CHECKS=0;"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            cur.close()
+
+            query = "INSERT INTO Rental_Equipment (rental_name, rental_cost) VALUES (%s,%s);"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (rental_name, rental_cost))
+            cur.close()
+
+            query = "SET FOREIGN_KEY_CHECKS=1;"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            cur.close()
+
+            mysql.connection.commit()
+
+            # redirect back to rental equipment page
+            return redirect("/Rental_Equipment")
+
+    # Grab Rental_Equipment data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the orders in Rental_Equipment
+        query = "SELECT * FROM Rental_Equipment;"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render rental equipment page passing our query data
+        return render_template("rental_equipment.j2", data=data)
+    
+
+# # route for delete functionality, deleting a rental equipment,
+# # we want to pass the 'id' value of that order on button click (see HTML) via the route
+@app.route("/delete_rental_equipment/<int:id>")
+def delete_rental_equipment(id):
+    # mySQL query to delete the rental equipment with our passed id
+    query = "DELETE FROM Rental_Equipment WHERE rental_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id,))
+    mysql.connection.commit()
+    # redirect back to rental equipment page
+    return redirect("/Rental_Equipment")
+
+
+# route for rental_orders page
+@app.route("/Orders", methods=["POST", "GET"])
+def order():
+    # Separate out the request methods, in this case this is for a POST
+    if request.method == "POST":
+        # fire off if user presses the Add order button
+        if request.form.get("Add_Order"):
+            # grab user form inputs
+            member_id = request.form["member_id"]
+            membership_id = request.form["membership_id"]
+            employee_id = request.form["employee_id"]
+
+            query = "SET FOREIGN_KEY_CHECKS=0;"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            cur.close()
+
+            if membership_id == "0":
+                query = "INSERT INTO Orders (member_id, membership_id, employee_id) VALUES (%s, NULL, %s);"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (member_id, employee_id))
+                cur.close()
+            else:
+                query = "INSERT INTO Orders (member_id, membership_id, employee_id) VALUES (%s, %s, %s);"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (member_id, membership_id, employee_id))
+                cur.close()
+
+            query = "SET FOREIGN_KEY_CHECKS=1;"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            cur.close()
+
+            mysql.connection.commit()
+
+            # redirect back to rental orders page
+            return redirect("/Orders")
+
+    # Grab Orders data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the orders in Orders
+        query = "SELECT * FROM Orders;"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # mySQL query to grab member id/name data for our dropdown
+        query1 = "SELECT member_id, member_name FROM Members ORDER BY member_id ASC;"
+        cur = mysql.connection.cursor()
+        cur.execute(query1)
+        member_data = cur.fetchall()
+
+        # mySQL query to grab membership id/name data for our dropdown
+        query2 = "SELECT membership_id, membership_name FROM Memberships ORDER BY membership_id ASC;"
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        membership_data = cur.fetchall()
+
+        # mySQL query to grab employee id/name data for our dropdown
+        query3 = "SELECT * FROM Employees ORDER BY employee_id ASC;"
+        cur = mysql.connection.cursor()
+        cur.execute(query3)
+        employee_data = cur.fetchall()
+
+        # render orders page passing our query data and additional datas to the order template
+        return render_template("orders.j2", data=data, members = member_data, memberships=membership_data, employees=employee_data)
+    
+# a route to expand order info
+@app.route("/view_order/<int:id>")
+def view_order(id):
+    # mySQL query to select expanded order info based on id
+    query = "SELECT Orders.*, Members.member_name, Memberships.membership_name, Memberships.membership_cost, Employees.employee_name FROM Orders LEFT JOIN Memberships ON Orders.membership_id = Memberships.membership_id LEFT JOIN Members ON Orders.member_id = Members.member_id LEFT JOIN Employees ON Orders.employee_id = Employees.employee_id WHERE Orders.order_id = %s;"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id,))
+    data = cur.fetchall()
+    return render_template("view_orders.j2", data = data)
+
+# # route for delete functionality, deleting a rental order,
+# # we want to pass the 'id' value of that order on button click (see HTML) via the route
+@app.route("/delete_order/<int:id>")
+def delete_order(id):
+    # mySQL query to delete the order with our passed id
+    query = "DELETE FROM Orders WHERE order_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id,))
+    mysql.connection.commit()
+    # redirect back to orders page
+    return redirect("/Orders")
+
+# # route for edit functionality, updating the attributes of an order in orders
+# # similar to our delete route, we want to the pass the 'id' value of that order on button click (see HTML) via the route
+@app.route("/edit_order/<int:id>", methods=["POST", "GET"])
+def edit_order(id):
+    if request.method == "GET":
+        # mySQL query to grab the info of the rental order with our passed id
+        query = "SELECT * FROM Orders WHERE order_id = '%s'" % (id)
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+        
+        # mySQL query to grab member id/name data for our dropdown
+        query1 = "SELECT member_id, member_name FROM Members ORDER BY member_id ASC;"
+        cur = mysql.connection.cursor()
+        cur.execute(query1)
+        member_data = cur.fetchall()
+
+        # mySQL query to grab membership id/name data for our dropdown
+        query2 = "SELECT membership_id, membership_name FROM Memberships ORDER BY membership_id ASC;"
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        membership_data = cur.fetchall()
+
+        # mySQL query to grab employee id/name data for our dropdown
+        query3 = "SELECT * FROM Employees ORDER BY employee_id ASC;"
+        cur = mysql.connection.cursor()
+        cur.execute(query3)
+        employee_data = cur.fetchall()       
+
+        # render edit__order page passing our query data to the edit_order
+        return render_template("edit_orders.j2", data=data, members = member_data, memberships=membership_data, employees=employee_data)
+    # meat and potatoes of our update functionality
+    if request.method == "POST":
+        # fire off if user clicks the 'Edit Order' button
+        if request.form.get("Edit_Order"):
+            # grab user form inputs
+            order_id = request.form["order_id"]
+            member_id = request.form["member_id"]
+            membership_id = request.form["membership_id"]
+            employee_id = request.form["employee_id"]
+
+            query = "SET FOREIGN_KEY_CHECKS=0;"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            cur.close()
+
+            if membership_id == "0":
+                query = "UPDATE Orders SET member_id = %s, membership_id = NULL, employee_id = %s WHERE order_id = %s"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (member_id, employee_id, order_id))
+                mysql.connection.commit()
+                cur.close()
+            else:
+                query = "UPDATE Orders SET member_id = %s, membership_id = %s, employee_id = %s WHERE order_id = %s"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (member_id, membership_id, employee_id, order_id))
+                mysql.connection.commit()
+                cur.close()
+
+            query = "SET FOREIGN_KEY_CHECKS=1;"
+            cur = mysql.connection.cursor()
+            cur.execute(query)
+            cur.close()
+
+            # redirect back to rental orders page after we execute the update query
+            return redirect("/Orders")
 
 # Listener
 # change the port number if deploying on the flip servers
 if __name__ == "__main__":
-    app.run(port=6536, debug=True)
+    app.run(port=6537, debug=True)
